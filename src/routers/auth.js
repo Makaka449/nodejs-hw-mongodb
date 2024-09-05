e687b24a6154fc52db82272122f6116665876f58
Makaka449 <145019010+Makaka449@users.noreply.github.com>
2024-09-05 18:51:26 +0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,9 @@
-// src/routers/auth.js
-const { loginUser } = require('../controllers/auth');
-const { validateLogin } = require('../middlewares/validate');
+import express from 'express';
+import { loginUser } from '../controllers/auth.js';
+import { validateLogin } from '../middlewares/validate.js';
+
+const router = express.Router();
 
 router.post('/login', validateLogin, loginUser);
+
+export default router;
